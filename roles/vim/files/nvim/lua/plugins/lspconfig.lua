90c2b78a026827f7b8a43da7e64299b2f6193141
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-10-15 17:37:18 -0600

    save lspconfig for go

---
+++ roles/vim/files/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    enabled = true,
+    opts = {
+      servers = {
+        gopls = {
+          on_attach = function(client, bufnr)
+            if vim.bo[bufnr].filetype == "go" then
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end
+          end,
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
