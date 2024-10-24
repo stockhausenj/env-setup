9ef47d9f26abf2bcdfe9bc72ac7e317025b0bdf7
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-10-24 10:05:01 -0600

    some macos tasks

---
+++ macos-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Install oh-my-zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install dependencies for running Ansible
+brew install ansible
