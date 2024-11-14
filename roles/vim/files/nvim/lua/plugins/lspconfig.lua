574a974724873a5166f4ad7afce0770fe067860a
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-11-13 22:57:25 -0700

    disable loading yaml schema store

--- roles/vim/files/nvim/lua/plugins/lspconfig.lua
+++ roles/vim/files/nvim/lua/plugins/lspconfig.lua
@@ -1,54 +1,59 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    enabled = true,
-    opts = {
-      servers = {
-        gopls = {
-          on_attach = function(client, bufnr)
-            if vim.bo[bufnr].filetype == "go" then
-              client.server_capabilities.documentFormattingProvider = true
-              client.server_capabilities.documentRangeFormattingProvider = true
-            end
-          end,
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = true,
-            },
-          },
-        },
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		enabled = true,
+		opts = {
+			servers = {
+				yamlls = {
+					schemaStore = {
+						enabled = false,
+					},
+				},
+				gopls = {
+					on_attach = function(client, bufnr)
+						if vim.bo[bufnr].filetype == "go" then
+							client.server_capabilities.documentFormattingProvider = true
+							client.server_capabilities.documentRangeFormattingProvider = true
+						end
+					end,
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								fieldalignment = true,
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					},
+				},
+			},
+		},
+	},
 }
